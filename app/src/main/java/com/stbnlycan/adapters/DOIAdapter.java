685c5ec147895e1d13f7998271255c46070cb74e
420042e <420042e@gmail.com>
2020-10-06 11:34:22 -0400

    Se añadió un menú para eliminar fotos

--- app/src/main/java/com/stbnlycan/adapters/DOIAdapter.java
+++ app/src/main/java/com/stbnlycan/adapters/DOIAdapter.java
@@ -1,22 +1,28 @@
 package com.stbnlycan.adapters;
 
+import android.annotation.SuppressLint;
 import android.media.Image;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuPopupHelper;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 import com.stbnlycan.controldeingreso.R;
 import com.stbnlycan.models.DocumentoIngreso;
-import com.stbnlycan.models.Visitante;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class DOIAdapter extends RecyclerView.Adapter<DOIAdapter.ARV>{
@@ -55,28 +61,88 @@ public class DOIAdapter extends RecyclerView.Adapter<DOIAdapter.ARV>{
         private ImageView doiImagen;
         private TextView tdoNombre;
         private DocumentoIngreso doi;
+        private ImageButton more;
 
         public ARV(@NonNull View itemView) {
             super(itemView);
             doiImagen = itemView.findViewById(R.id.doiImagen);
             tdoNombre = itemView.findViewById(R.id.tdoNombre);
+            more = itemView.findViewById(R.id.more);
 
             itemView.setOnClickListener(this);
+
+            final View itemView2 = itemView;
+
+            more.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("RestrictedApi")
+                @Override
+                public void onClick(View v) {
+                    PopupMenu popup = new PopupMenu(more.getContext(), itemView2);
+
+                    popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                        @Override
+                        public boolean onMenuItemClick(MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.action_eliminar:
+                                    //moveFile(recordName.getText().toString(), getAdapterPosition());
+                                    Log.d("Click","clickeado"+getAdapterPosition());
+                                    doiList.remove(getAdapterPosition());
+                                    notifyItemRemoved(getAdapterPosition());
+                                    notifyItemRangeChanged(getAdapterPosition(),doiList.size());
+                                    return true;
+                                /*case R.id.action_play:
+                                    String valueOfPath = recordName.getText().toString();
+                                    Intent intent = new Intent();
+                                    intent.setAction(android.content.Intent.ACTION_VIEW);
+                                    File file = new File(valueOfPath);
+                                    intent.setDataAndType(Uri.fromFile(file), "audio/*");
+                                    context.startActivity(intent);
+                                    return true;
+                                case R.id.action_share:
+                                    String valueOfPath = recordName.getText().toString();
+                                    File filee = new File(valueOfPath);
+                                    try {
+                                        Intent sendIntent = new Intent();
+                                        sendIntent.setAction(Intent.ACTION_SEND);
+                                        sendIntent.setType("audio/*");
+                                        sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(filee));
+                                        context.startActivity(sendIntent);
+                                    } catch (NoSuchMethodError | IllegalArgumentException | NullPointerException e) {
+                                        e.printStackTrace();
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    return true;*/
+                                default:
+                                    return false;
+                            }
+                        }
+                    });
+                    // here you can inflate your menu
+                    popup.inflate(R.menu.menu_foto);
+                    popup.setGravity(Gravity.RIGHT);
+
+                    // if you want icon with menu items then write this try-catch block.
+                    try {
+                        Field mFieldPopup=popup.getClass().getDeclaredField("mPopup");
+                        mFieldPopup.setAccessible(true);
+                        MenuPopupHelper mPopup = (MenuPopupHelper) mFieldPopup.get(popup);
+                        mPopup.setForceShowIcon(true);
+                    } catch (Exception e) {
+
+                    }
+                    popup.show();
+                }
+            });
         }
 
         @Override
         public void onClick(View view)
         {
-            Log.d("Click","clickeado"+getAdapterPosition());
+            /*Log.d("Click","clickeado"+getAdapterPosition());
             doiList.remove(getAdapterPosition());
             notifyItemRemoved(getAdapterPosition());
-            notifyItemRangeChanged(getAdapterPosition(),doiList.size());
-            /*if(getAdapterPosition() == 0)
-            {
-                doiList.remove(getAdapterPosition());
-                notifyItemRemoved(getAdapterPosition());
-                notifyItemRangeChanged(getAdapterPosition(),doiList.size());
-            }*/
+            notifyItemRangeChanged(getAdapterPosition(),doiList.size());*/
             doiListener.OnDOIClick(doi);
         }
     }
